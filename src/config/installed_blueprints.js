5123ccbb67702c1e5f5e48f3eda9b6de706f01a8
Crowdbotics <team@crowdbotics.com>
2021-01-21 13:55:39 +0000

    add package blueprint-react-native-sign-in-5-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn526195072', human_name: 'Sign In 5 26', access_route: 'SignIn526195072'},
 { name: 'SignUp127195071', human_name: 'Sign Up 1 27', access_route: 'SignUp127195071'},
 { name: 'SignUp228195070', human_name: 'Sign Up 2 28', access_route: 'SignUp228195070'},
 { name: 'UserProfile31195067', human_name: 'User Profile 31', access_route: 'UserProfile31195067'},
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn526195072Reducer from '../features/SignIn526195072/redux/reducers'
 import SignUp127195071Reducer from '../features/SignUp127195071/redux/reducers'
 import SignUp228195070Reducer from '../features/SignUp228195070/redux/reducers'
 
@@ -17,6 +18,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn526195072: SignIn526195072Reducer,
 SignUp127195071: SignUp127195071Reducer,
 SignUp228195070: SignUp228195070Reducer,
 
